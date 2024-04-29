02011e249f4637568c0e8200f01555029a4c4f23
ThanisAlbert <thanisalbert@gmail.com>
2024-04-29 13:56:39 +0530

    Pdf_to_excel_extraction_done

---
+++ py.py
@@ -0,0 +1,8 @@
+import pdb; 
+import os
+#directory = os.getcwd()
+#pdb.set_trace()
+#print("testing")
+#print(directory)
+os.chdir("D:\Macro\Redgulf\Redbot1627_Rev_Powerquery")
+os.system("templog.bat")
